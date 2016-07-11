c74c06642c1f57b0957cc8d0bfa5dbd56f1cfa8e
Daniel Herr <DanielHerr@users.noreply.github.com>
2016-07-11 01:56:07 -0400

    Add Once Option Support
    Return Promise for Once without Listener

--- on.js
+++ on.js
@@ -1,38 +1,56 @@
 "use strict"
 
-EventTarget.prototype.on = function(events = [] || "", options = {}, listener) {
+EventTarget.prototype.on = function(event = [] || "", options = {}, listener) {
  let target = this
  if(listener == undefined && typeof(options) == "function") {
   listener = options
-  options = undefined
+  options = {}
  }
- if(typeof(events) == "string") {
-  events = [ events ]
- }
- for(let event of events) {
-  function newlistener(...inputs) {
-   return(listener.apply(this, inputs))
-  }
-  newlistener.remove = function() {
-   target.removeEventListener(this.event, newlistener, this.options)
-   target.listeners[this.event].splice(target.listeners[this.event].indexOf(newlistener), 1)
-   if(target.listeners[this.event].length == 0) {
-    delete(target.listeners[this.event])
+ if(typeof(event) == "string") {
+  let promise
+  if(options.once) {
+   if(listener == null) {
+    promise = new Promise(function(resolve) {
+     listener = resolve
+    })
+   } else {
+    let listenersource = listener.toString()
+    listener = new Proxy(listener, {
+     apply(target, that, inputs) {
+      listener.remove()
+      return(Reflect.apply(target, that, inputs))
+    } })
+    listener.toString = function() {
+     return(listenersource)
+  } } }
+  listener.target = target
+  listener.event = event
+  listener.options = options
+  listener.remove = function() {
+   target.removeEventListener(event, listener, options)
+   target.listeners[event].splice(target.listeners[event].indexOf(listener), 1)
+   if(target.listeners[event].length == 0) {
+    delete(target.listeners[event])
   } }
-  Object.defineProperties(newlistener, {
-   name: {
-    value: listener.name, configurable: true
-   }, length: {
-    value: listener.length, configurable: true
-  } })
-  target.addEventListener(event, newlistener, options)
+  target.addEventListener(event, listener, options)
   if(target.listeners == null) {
    target.listeners = {}
   }
   if(target.listeners[event] == null) {
    target.listeners[event] = []
   }
-  newlistener.event = event
-  newlistener.options = options
-  target.listeners[event].push(newlistener)
+  target.listeners[event].push(listener)
+  return(promise || listener)
+ } else {
+  let events = event
+  let listeners = []
+  for(let event of events) {
+   let listenersource = listener.toString()
+   let newlistener = new Proxy(listener, {})
+   newlistener.toString = function() {
+    return(listenersource)
+   }
+   listeners.push(target.on(event, options, newlistener))
+  }
+  return(listeners)
 } }
